2a7f1098a3465688e4d3863af5bb5d1f1183e903
Alex Hurley <the-Hull@users.noreply.github.com>
2019-04-04 17:04:28 +0100

    clean up

--- push_back.R
+++ push_back.R
@@ -1,36 +1,22 @@
-# push results back into repo
-# - Rscript push_back.R
-
 # set up repo and add remote
 repo <- git2r::repository(".")
+
 # repo
 repo_url <- "https://github.com/the-Hull/02_task_automation.git"
-#
+
 git2r::remote_add(repo, name = "taskauto", url = repo_url)
-#
-#
-#
+
 git2r::config(repo,
               user.email = "aghu@aghu.com",
               user.name = "travishull")
 
-#
-# travis_build <- Sys.getenv("TRAVIS_BUILD_NUMBER")
 commit_message <- paste("Update reports: Travis Build")
-# cred <- Sys.getenv("GH_TOKEN")
 cred <- git2r::cred_token("GH_TOKEN")
 
-#
-#
-#
-#
+
 git2r::checkout(repo, branch = "master")
-message("here")
 
 last_commit_author <- git2r::commits(repo = repo)[[1]]$author[[1]]
-message("auth")
-
-message(last_commit_author)
 
 
 if (last_commit_author != "travishull"){
@@ -52,7 +38,5 @@ if (last_commit_author != "travishull"){
     # Mt St Helens
     # Grand Teton Mountain
     # El Capitan Yosemite
-    # git remote add origin-rhydro https://${GH_TOKEN}@github.com/the-Hull/02_task_automation.git
-    # git push origin-rhydro
 
 }
